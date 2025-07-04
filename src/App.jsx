c38c10e2f3b580fb5406fb6b84c7f6a075c6bb3c
anganbalaishram <anganbalaishram59@gmail.com>
2025-07-04 16:04:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
+import { ToastContainer } from 'react-toastify';
+
+
+const App = () => {
+  return (
+    <div>
+      <ToastContainer/>
+      <Header />
+      <About />
+      <Projects />
+      <Testimonials />
+      <Contact />
+      <Footer/>
+    </div>
+  )
+};
+
+export default App;
